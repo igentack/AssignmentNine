58e54cb4798feed35c9b2da184ca10cfbd0ed8c0
satisfly <vilkenbraadress@gmail.com>
2022-12-08 09:45:00 +0100

    first bish bosh

---
+++ bishbosh.js
@@ -0,0 +1,14 @@
+let bishCount = 100;
+
+for (let index = 0; index <= bishCount; index++) {
+    if (index % 3 == 0 && index % 4 == 0) {
+        console.log("Bish-Bosh");
+    } else if (index % 3 == 0) {
+        console.log("Bish");
+    } else if (index % 4 == 0) {
+        console.log("Bosh");
+        
+    } else {
+        console.log(index);
+    }  
+}
